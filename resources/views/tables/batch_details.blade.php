b5e5e474918f7195fa4ce148c322f283acb4d0d9
Bakasa Joshua <joshua.bakasa@dataposit.co.ke>
2018-07-10 16:49:58 +0300

    Debug server

--- resources/views/tables/batch_details.blade.php
+++ resources/views/tables/batch_details.blade.php
@@ -12,26 +12,168 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-4">
-<<<<<<< HEAD
-                            <p><strong>Batch:</strong> {{ $data->batch->id  ?? '' }}</p>
-                        </div>
-                        <div class="col-md-8">
-                            <p><strong>Facility:</strong> {{-- ($data->batch->view_facility->facilitycode . ' - ' . $data->batch->view_facility->name . ' (' . $data->batch->view_facility->county . ')') ?? '' --}}</p>
-=======
                             <p><strong>Batch:</strong> {{ $batch->id  ?? '' }}</p>
                         </div>
                         <div class="col-md-8">
                             <p><strong>Facility:</strong> {{ ($batch->view_facility->facilitycode . ' - ' . $batch->view_facility->name . ' (' . $batch->view_facility->county . ')') ?? '' }}</p>
->>>>>>> 5ac1971fe54e86743685d3c274c157db16da65ac
                         </div>
                         <div class="col-md-4">
                             <p>
                                 <strong>Entry Type: </strong>
-<<<<<<< HEAD
-                                @switch($data->batch->site_entry)
-=======
                                 @switch($batch->site_entry)
->>>>>>> 5ac1971fe54e86743685d3c274c157db16da65ac
+                                    @case(0)
+                                        {{ 'Lab Entry' }}
+                                        @break
+                                    @case(1)
+                                        {{ 'Site Entry' }}
+                                        @break
+                                    @case(2)
+                                        {{ 'POC Entry' }}
+                                        @break
+                                    @default
+                                        @break
+                                @endswitch
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <p><strong>Date Entered:</strong> {{ $batch->my_date_format('created_at') }}</p>
+                        </div>
+                        <div class="col-md-4">
+                            <p><strong>Entered By:</strong> 
+                                @if($batch->creator)
+                                    @if($batch->creator->full_name != ' ')
+                                        {{ $batch->creator->full_name }}
+                                    @else
+                                        {{ $batch->creator->facility->name ?? '' }}
+                                    @endif
+                                @endif
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <p><strong>Date Received:</strong> {{ $batch->my_date_format('datereceived')  ?? '' }}</p>
+                        </div>
+                        <div class="col-md-8">
+                            <p><strong>Received By:</strong> {{ $batch->receiver->full_name ?? '' }}</p>
+                        </div>                       
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover" >
+                            <thead>
+                                <tr>
+                                    <th colspan="17"><center> Sample Log</center></th>
+                                </tr>
+                                <tr>
+                                    <th colspan="6">Patient Information</th>
+                                    <th colspan="3">Sample Information</th>
+                                    <th colspan="7">Mother Information</th>
+                                </tr>
+                                <tr> 
+                                    <th>No</th>
+                                    <th>Patient ID</th>
+                                    <th>Sex</th>
+                                    <th>DOB</th>
+                                    <th>Age (Months)</th>
+                                    <th>Infant Prophylaxis</th>
+
+                                    <th>Date Collected</th>
+                                    <th>Status</th>
+                                    <th>Spots</th>
+
+                                    <th>CCC #</th>
+                                    <th>Age</th>
+                                    <th>Last Vl</th>
+                                    <th>PMTCT Intervention</th>
+                                    <th>Feeding Type</th>
+                                    <th>Entry Point</th>
+                                    <th>Result</th>
+                                </tr>
+                            </thead>
+                            <tbody> 
+                                @foreach($batch->sample as $key => $sample)
+                                    <tr>
+                                        <td> {{ $key+1 }} </td>
+                                        <td> {{ $sample->patient->patient }} </td>
+                                        <td> 
+                                            @foreach($data->genders as $gender)
+                                                @if($sample->patient->sex == $gender->id)
+                                                    {{ $gender->gender_description }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td> {{ $sample->patient->my_date_format('dob') }} </td>
+                                        <td> {{ $sample->age }} </td>
+                                        <td>
+                                            @foreach($data->iprophylaxis as $iproph)
+                                                @if($sample->regimen == $iproph->id)
+                                                    {{ $iproph->name }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+
+                                        <td> {{ $sample->my_date_format('datecollected') }} </td>
+                                        <td>
+                                            @foreach($data->received_statuses as $received_status)
+                                                @if($sample->receivedstatus == $received_status->id)
+                                                    {{ $received_status->name }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td> {{ $sample->spots }} </td>
+
+                                        <td> {{ $sample->patient->mother->ccc_no ?? '' }} </td>
+                                        <td> {{ $sample->mother_age }} </td>
+                                        <td> {{ $sample->mother_last_result }} </td>
+                                        <td>
+                                            @foreach($data->interventions as $intervention)
+                                                @if($sample->mother_prophylaxis == $intervention->id)
+                                                    {{ $intervention->name }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td>
+                                            @foreach($data->feedings as $feeding)
+                                                @if($sample->feeding == $feeding->id)
+                                                    {{ $feeding->feeding }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td>
+                                            @foreach($data->entry_points as $entry_point)
+                                                @if($sample->patient->entry_point == $entry_point->id)
+                                                    {{ $entry_point->name }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td>
+                                            @foreach($data->results as $result)
+                                                @if($sample->result == $result->id)
+                                                    {{ $result->name }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+@section('scripts') 
+
+    @component('/tables/scripts')
+
+    @endcomponent
+
+@endsection
                                     @case(0)
                                         {{ 'Lab Entry' }}
                                         @break
