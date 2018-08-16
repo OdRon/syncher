44fb20e8d14bbe187976c2bf17c42bf8526f5b2c
Unknown <baksajoshua09@gmail.com>
2018-08-16 13:43:15 +0300

    Broke down  No data report
    Broke down the no data report under Maryland to individual sample reports

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -24,6 +24,8 @@
             <li><a href="{{ url('user/passwordReset') }}">Change Password</a></li>
             <hr />
         @elseif(Auth::user()->user_type_id == 9)
+            <li><a href="{{ url('reports') }}">Reports Download</a></li>
+            <hr />
             <li><a href="{{ url('reports/nodata/EID') }}">EID No Data Reports</a></li>
             <hr />
             <li><a href="{{ url('reports/nodata/VL') }}">VL No Data Reports</a></li>
