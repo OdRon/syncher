350177f3ff5b6da80064a9db3597c1a384a50006
Unknown <baksajoshua09@gmail.com>
2018-09-03 08:23:32 +0300

    Resize Logo on the Top navigation

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -19,7 +19,7 @@
     </div>
     <div id="logo" class="light-version" style="padding-left: 2px; padding-top: 6px; width: 250px;">
         <span>
-            <img src="{{ asset(env('LOGO')) }}">
+            <img src="{{ asset(env('LOGO')) }}" style="width: 250px;">
         </span>
     </div>
     <nav role="navigation">
