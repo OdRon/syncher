92500daef9d1ddbeb9a051a4abe3e4a817db48d5
Unknown <baksajoshua09@gmail.com>
2018-09-03 11:07:34 +0300

    Remove use of assumed constant

--- resources/views/mail/passwordAnnouncement.blade.php
+++ resources/views/mail/passwordAnnouncement.blade.php
@@ -1,13 +1,13 @@
 <p>
-	Hello {{ $credentials.name }},
+	Hello {{ $credentials->name }},
 	<br />
 	The EID/VL system was undergoing maintenance from Thursday 30, 2018. Major changes have been done to the system including changes to the data encryption to the database. For this reason passwords have been changed/
 	<br />
 
-	Please Log in to <a href='http://eiddash.nascop.org/'></a> with your usual username but with a password of {{ $credentials.password }}. Once you log in please change you password to your desired password.
+	Please Log in to <a href='http://eiddash.nascop.org/'></a> with your usual username but with a password of {{ $credentials->password }}. Once you log in please change you password to your desired password.
 	<br />
 
-	Please note your registered email is {{ $credentials.email }}
+	Please note your registered email is {{ $credentials->email }}
 	<br />
 
 	Thank you for your patience.
