39067018caccee8ea70cb27c8baae947781235c1
Unknown <baksajoshua09@gmail.com>
2018-09-03 11:30:40 +0300

    Server Debug

--- app/Copier.php
+++ app/Copier.php
@@ -322,7 +322,7 @@ class Copier
             if($oldUsers->isEmpty()) break;
             echo "==> Started at " . date('d/m/Y h:i:s a', time()). "\n";
             foreach ($oldUsers as $key => $value) {
-                $current = User::where('username', '=', $value->username)->get();
+                $current = User::where('username', '=', $value->username);
                 if(!$current->isEmpty()) {
                     $current->deleted_at = date('Y-m-d H:i:s');
                     $current->save();
