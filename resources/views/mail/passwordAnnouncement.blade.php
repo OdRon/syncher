8f976e69c3b5067ff8e1187c1c92399249418a26
Unknown <baksajoshua09@gmail.com>
2018-09-03 11:09:40 +0300

    Formating the email

--- resources/views/mail/passwordAnnouncement.blade.php
+++ resources/views/mail/passwordAnnouncement.blade.php
@@ -1,13 +1,16 @@
 <p>
 	Hello {{ $credentials->name }},
 	<br />
-	The EID/VL system was undergoing maintenance from Thursday 30, 2018. Major changes have been done to the system including changes to the data encryption to the database. For this reason passwords have been changed/
+	The EID/VL system was undergoing maintenance from Thursday 30, 2018. Major changes have been done to the system including changes to the data encryption to the database. For this reason passwords have been changed.
+	<br />
 	<br />
 
-	Please Log in to <a href='http://eiddash.nascop.org/'></a> with your usual username but with a password of {{ $credentials->password }}. Once you log in please change you password to your desired password.
+	Please Log in to <a href='http://eiddash.nascop.org/'>National</a> with your usual username but with a password of {{ $credentials->password }}. Once you log in please change you password to your desired password.
+	<br />
 	<br />
 
 	Please note your registered email is {{ $credentials->email }}
+	<br />
 	<br />
 
 	Thank you for your patience.
