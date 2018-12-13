3e7823eef7de75a16a9f55ef0ab0d8a7564533e7
Joel <joelkith@gmail.com>
2018-12-13 14:33:41 +0300

    Add Emailing Functionality

--- resources/views/emails/lab_signature.blade.php
+++ resources/views/emails/lab_signature.blade.php
@@ -1,10 +1,12 @@
-<p>
-	<br />
-	Regards, <br />
-	{{ $lab->labname }} <br />
-	{{ $lab->name }}  <br />
-	{{ $lab->lablocation }} <br />
-	{{ $lab->labtel1 }}  <br />
-	{{ $lab->labtel2 }}  <br />
-	{{ $lab->email }}  <br />
-</p>
+@if($lab)
+	<p>
+		<br />
+		Regards, <br />
+		{{ $lab->labname }} <br />
+		{{ $lab->name }}  <br />
+		{{ $lab->lablocation }} <br />
+		{{ $lab->labtel1 }}  <br />
+		{{ $lab->labtel2 }}  <br />
+		{{ $lab->email }}  <br />
+	</p>
+@endif
