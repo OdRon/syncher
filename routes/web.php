9c0546bbb105697aa30a96871ccb5572e85b1597
Joel <joelkith@gmail.com>
2018-12-13 14:17:11 +0300

    Add Emailing Functionality

--- routes/web.php
+++ routes/web.php
@@ -39,11 +39,14 @@ Route::post('facility/search/', 'FacilityController@search')->name('facility.sea
 Route::middleware(['web', 'auth'])->group(function(){
 	Route::get('/home', 'HomeController@index')->name('home');
 
-	Route::prefix('email')->name('email.')->group(function () {
-		Route::get('preview/{email}', 'EmailController@demo')->name('demo');
-		Route::post('preview/{email}', 'EmailController@demo_email')->name('demo_email');
+	Route::group(['middleware' => ['only_utype:10,12']], function () {
+		Route::prefix('email')->name('email.')->group(function () {
+			Route::get('preview/{email}', 'EmailController@demo')->name('demo');
+			Route::post('preview/{email}', 'EmailController@demo_email')->name('demo_email');
+		});
+		Route::resource('email', 'EmailController');
 	});
-	Route::resource('email', 'EmailController');
+
 
 	Route::prefix('hei')->name('hei.')->group(function(){
 		Route::get('validate/{year?}/{month?}', 'HEIController@index')->name('followup');
