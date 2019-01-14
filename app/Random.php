d9553d0ddd9c2340d6bdd120f2befbe67da5c4da
Joel <joelkith@gmail.com>
2019-01-14 18:21:06 +0300

    Updates

--- app/Random.php
+++ app/Random.php
@@ -55,10 +55,10 @@ class Random
 			foreach ($samples as $sample) {
 				if($sample->id == $original->id) continue;
 
-				// if($sample->worksheet_id == $original->worksheet_id  && $sample->result == $original->result){
+				if($sample->worksheet_id == $original->worksheet_id  && $sample->interpretation == $original->interpretation && $sample->batch_id == $original->batch_id){
 					$s = $sample_model::find($sample->id);
 					$s->delete();
-				// }
+				}
 			}
 		}
 	}
