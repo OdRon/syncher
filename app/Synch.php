73681d448b59edd87a5f97eaf1fee56ea52f8beb
Joel <joelkith@gmail.com>
2019-01-23 13:28:47 +0300

    Synch Updates

--- app/Synch.php
+++ app/Synch.php
@@ -62,6 +62,7 @@ class Synch
 		try {
 			$response = $client->request('post', 'auth/login', [
 	            'http_errors' => false,
+	            'connect_timeout' => 3.14,
 				'headers' => [
 					'Accept' => 'application/json',
 				],
