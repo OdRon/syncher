446242ba1fb973a6ccbed3a4e3ec255e697933e2
Unknown <baksajoshua09@gmail.com>
2019-01-27 09:16:24 +0300

    Addition of the tables in preparation of allocations

---
+++ app/Allocation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Allocation extends Model
+{
+    /**
+     * The attributes that should be guarded from mass assignment.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+}
