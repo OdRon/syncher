13bdba716ad44590cd444cc9440249a4d8cccedf
Joel <joelkith@gmail.com>
2019-02-21 17:36:15 +0300

    Partner Pull

--- app/Api/V1/Controllers/PullController.php
+++ app/Api/V1/Controllers/PullController.php
@@ -5,8 +5,8 @@ namespace App\Api\V1\Controllers;
 use App\Http\Controllers\Controller;
 use App\Api\V1\Requests\BlankRequest;
 
-use \App\SampleView;
-use \App\ViralsampleView;
+use \App\SampleCompleteView;
+use \App\ViralsampleCompleteView;
 
 class PullController extends Controller
 {
@@ -23,7 +23,7 @@ class PullController extends Controller
         $ids = $request->input('ids');   
 
         // if($test == 2){
-            $class = SampleView::class;
+            $class = SampleCompleteView::class;
             $table = 'samples_view';
         // }
         // else if($test == 1){
@@ -94,8 +94,8 @@ class PullController extends Controller
         $dispatched = $request->input('dispatched');   
         $ids = $request->input('ids'); 
 
-        $class = ViralsampleView::class;
-        $table = 'viralsamples_view';
+        $class = ViralsampleCompleteView::class;
+        $table = 'viralsample_complete_view';
 
         if($patients){
             $patients = str_replace(' ', '', $patients);
@@ -110,8 +110,10 @@ class PullController extends Controller
             $facilities = explode(',', $facilities);
         }
  
-        $result = $class::select("{$table}.*", 'view_facilitys.facilitycode')
+        // $result = $class::select("{$table}.*", 'view_facilitys.facilitycode', 'labs.name as labname')
+        $result = $class::selectRaw("{$table}.id AS `system_id`, original_batch_id AS `batch`, patient AS `ccc_number`, labs.name AS `lab_tested_in`, county, subcounty, partner, view_facilitys.name AS `facility`, facilitycode AS `facility_code`, gender_description AS `gender`, dob, age, sampletype_name as `sample_type`, datecollected as `date_collected`, justification_name as `justification`, datereceived as `date_received`, datetested as `date_tested`, datedispatched as `date_dispatched`, initiation_date as `art_initiation_date`, receivedstatus_name AS `received_status`, rejected_name as `rejected_reason`, prophylaxis_name as `regimen`, regimenline as `regimen_line`, pmtct_name as `pmtct`, result   ")
             ->join('view_facilitys', 'view_facilitys.id', '=', "{$table}.facility_id")
+            ->join('labs', 'labs.id', '=', "{$table}.lab_id")
             ->when($facilities, function($query) use($facilities){
                 return $query->whereIn('facilitycode', $facilities);
             })
@@ -131,21 +133,29 @@ class PullController extends Controller
             ->orderBy('datecollected', 'desc')
             ->paginate(50);
 
-        $result->transform(function ($sample, $key){
-
-            return [        
-                'lab_id' => $sample->id,
-                'patient_id' => $sample->patient,
-                'MFLCode' => $sample->facility_code,
-                'date_collected' => $sample->datecollected,
-                'date_received' => $sample->datereceived,
-                'date_tested' => $sample->datetested,
-                'date_dispatched' => $sample->datedispatched,
-                'result' => $sample->result_name,
-            ];
-        });
-
         return $result;
+
+        // $result->transform(function ($sample, $key){
+
+        //     return [        
+        //         'system_id' => $sample->id,
+        //         'batch' => $sample->original_batch_id,
+        //         'ccc_number' => $sample->patient,
+        //         'lab_tested_in' => $sample->labname,
+        //         'county' => $sample->county,
+        //         'sub'
+
+
+        //         'lab_id' => $sample->id,
+        //         'patient_id' => $sample->patient,
+        //         'MFLCode' => $sample->facility_code,
+        //         'date_collected' => $sample->datecollected,
+        //         'date_received' => $sample->datereceived,
+        //         'date_tested' => $sample->datetested,
+        //         'date_dispatched' => $sample->datedispatched,
+        //         'result' => $sample->result_name,
+        //     ];
+        // });
     }
 
 }
