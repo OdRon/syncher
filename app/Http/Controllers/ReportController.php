a6a181301cd4c43ec757fb71356e26342487ca60
Bakasa Joshua <baksajoshua09@gmail.com>
2019-03-04 15:27:18 +0300

    Server debug

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -231,7 +231,7 @@ class ReportController extends Controller
             foreach($lab->where('id', $data->lab_id) as $labselect) {
                 $newlab = $labselect;
             }
-            dd($newlab);
+            dd($newlab->id);
             $dbData->lab_name = $lab->labname;
         }
         // foreach ($lab as $labkey => $labvalue) {
