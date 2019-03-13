6c9c79394d58c87047a35cfef75625374f05871a
Joel <joelkith@gmail.com>
2019-03-14 00:22:58 +0300

    Updates

--- app/Synch.php
+++ app/Synch.php
@@ -378,7 +378,10 @@ class Synch
 						$patient->sex = $body->patient->sex;
 						$patient->save();
 					}
-				}				
+				}	
+				else{
+					print_r($body);
+				}			
 			} catch (Exception $e) {
 				
 			}
