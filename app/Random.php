19aba92679d3bf0a5f9fe2f2d70256fe5765bc97
Bakasa Joshua <baksajoshua09@gmail.com>
2019-03-14 16:11:57 +0300

    Less than 2 months

--- app/Random.php
+++ app/Random.php
@@ -1533,88 +1533,89 @@ class Random
 	}
 
 	public static function negatives_report($year=2018, $month=null){
-        // // echo "Method start \n";
-        // ini_set("memory_limit", "-1");
-    	// if($year==null){
-    	// 	$year = Date('Y');
-    	// }
+        // echo "Method start \n";
+        ini_set("memory_limit", "-1");
+    	if($year==null){
+    		$year = Date('Y');
+    	}
 
-    	// $raw = "samples_view.id, samples_view.patient, samples_view.facility_id, labs.name as lab, view_facilitys.name as facility_name, view_facilitys.county, samples_view.pcrtype,  datetested";
-    	// $raw2 = "samples_view.id, samples_view.patient, samples_view.facility_id, samples_view.pcrtype, datetested";
+    	$raw = "samples_view.id, samples_view.patient, samples_view.facility_id, labs.name as lab, view_facilitys.name as facility_name, view_facilitys.county, samples_view.pcrtype,  datetested";
+    	$raw2 = "samples_view.id, samples_view.patient, samples_view.facility_id, samples_view.pcrtype, datetested";
 
-    	// $data = DB::table("samples_view")
-		// ->select(DB::raw($raw))
-		// ->join('view_facilitys', 'samples_view.facility_id', '=', 'view_facilitys.id')
-		// ->join('labs', 'samples_view.lab_id', '=', 'labs.id')
-		// ->orderBy('samples_view.facility_id', 'desc')
-		// ->whereYear('datetested', $year)
-		// ->when($month, function($query) use ($month){
-		// 	if($month != null || $month != 0){
-		// 		return $query->whereMonth('datetested', $month);
-		// 	}
-		// })
-		// ->where('result', 1)
-		// ->where('samples_view.repeatt', 0)
-		// ->where('samples_view.flag', 1)
-		// ->where('samples_view.eqa', 0)
-		// ->get();
+    	$data = DB::table("samples_view")
+		->select(DB::raw($raw))
+		->join('view_facilitys', 'samples_view.facility_id', '=', 'view_facilitys.id')
+		->join('labs', 'samples_view.lab_id', '=', 'labs.id')
+		->orderBy('samples_view.facility_id', 'desc')
+		->whereYear('datetested', $year)
+		->when($month, function($query) use ($month){
+			if($month != null || $month != 0){
+				return $query->whereMonth('datetested', $month);
+			}
+		})
+		->where('result', 1)
+		->where('samples_view.repeatt', 0)
+		->where('samples_view.flag', 1)
+		->where('samples_view.eqa', 0)
+		->where('age', '<', '2.01')
+		->get();
 
-		// // echo "Total {$data->count()} \n";
+		// echo "Total {$data->count()} \n";
 
-		// $i = 0;
-		// $result = null;
+		$i = 0;
+		$result = null;
 
-		// foreach ($data as $patient) {
+		foreach ($data as $patient) {
 
-	    // 	$d = DB::table("samples_view")
-		// 	->select(DB::raw($raw2))
-		// 	->where('facility_id', $patient->facility_id)
-		// 	->where('patient', $patient->patient)
-		// 	->where('datetested', '<', $patient->datetested)
-		// 	->where('result', 2)
-		// 	->where('repeatt', 0)
-		// 	->where('flag', 1)
-		// 	->where('eqa', 0)
-		// 	->first();
+	    	$d = DB::table("samples_view")
+			->select(DB::raw($raw2))
+			->where('facility_id', $patient->facility_id)
+			->where('patient', $patient->patient)
+			->where('datetested', '<', $patient->datetested)
+			->where('result', 2)
+			->where('repeatt', 0)
+			->where('flag', 1)
+			->where('eqa', 0)
+			->first();
 
-		// 	if($d){
-		// 		$result[$i]['laboratory'] = $patient->lab;
-        //         $result[$i]['facility'] = $patient->facility_id;
-        //         $result[$i]['county'] = $patient->county;
-		// 		$result[$i]['patient_id'] = $patient->patient;
+			if($d){
+				$result[$i]['laboratory'] = $patient->lab;
+                $result[$i]['facility'] = $patient->facility_id;
+                $result[$i]['county'] = $patient->county;
+				$result[$i]['patient_id'] = $patient->patient;
 
-		// 		$result[$i]['negative_sample_id'] = $patient->id; 
-		// 		$result[$i]['negative_date'] = $patient->datetested;
-		// 		$result[$i]['negative_pcr'] = $patient->pcrtype;
+				$result[$i]['negative_sample_id'] = $patient->id; 
+				$result[$i]['negative_date'] = $patient->datetested;
+				$result[$i]['negative_pcr'] = $patient->pcrtype;
 
-		// 		$result[$i]['positive_sample_id'] = $d->id;
-		// 		$result[$i]['positive_date'] =  $d->datetested;
-		// 		$result[$i]['positive_pcr'] = $d->pcrtype;
-		// 		$i++;
+				$result[$i]['positive_sample_id'] = $d->id;
+				$result[$i]['positive_date'] =  $d->datetested;
+				$result[$i]['positive_pcr'] = $d->pcrtype;
+				$i++;
 
-		// 		// echo "Found 1 \n";
-		// 		$d = null;
-		// 	}
+				// echo "Found 1 \n";
+				$d = null;
+			}
 
 
-		// }
+		}
 		$file = $year . 'Positive_to_Negative';
-		// Excel::create($file, function($excel) use($result)  {
+		Excel::create($file, function($excel) use($result)  {
 
-		//     // Set sheets
+		    // Set sheets
 
-		//     $excel->sheet('Sheetname', function($sheet) use($result) {
+		    $excel->sheet('Sheetname', function($sheet) use($result) {
 
-		//         $sheet->fromArray($result);
+		        $sheet->fromArray($result);
 
-		//     });
+		    });
 
-		// })->store('csv');
+		})->store('csv');
 
 		
 
-		$data = [storage_path("exports/" . $file . ".csv")];
+		// $data = [storage_path("exports/" . $file . ".csv")];
 
-		Mail::to(['baksajoshua09@gmail.com'])->send(new TestMail($data));
+		// Mail::to(['baksajoshua09@gmail.com', 'joshua.bakasa@dataposit.co.ke'])->send(new TestMail($data));
     }
 }
