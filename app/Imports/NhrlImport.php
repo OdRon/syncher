a77678c569cef175863e8fa3ae9a7e7a6f9fbbc2
Joshua Bakasa <baksajoshua09@gmail.com>
2019-06-11 10:48:07 +0300

    Debug import and creation of export class

--- app/Imports/NhrlImport.php
+++ app/Imports/NhrlImport.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Imports;
 
+use Excel;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use App\Exports\NhrlExport;
 // use Illuminate\Contracts\Queue\ShouldQueue;
 // use Maatwebsite\Excel\Concerns\WithChunkReading;
 
@@ -10,7 +12,9 @@ class NhrlImport implements ToCollection/*, WithChunkReading, ShouldQueue*/
 {
 	public function collection(Collection $rows)
     {
-        dd($rows);
+        foreach ($rows as $key => $row) {
+        	dd($row);
+        }
     }
     
     // public function chunkSize(): int
