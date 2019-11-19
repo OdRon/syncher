b3b481b1a41cfb5730702291a7754cf9a6130a7b
Joel <joelkith@gmail.com>
2019-11-19 11:21:37 +0300

    Updates.

--- app/Report.php
+++ app/Report.php
@@ -83,7 +83,7 @@ class Report
 
 	        	if(str_contains($column_name, ['ccc', 'mainrecipientmail']) && filter_var($value, FILTER_VALIDATE_EMAIL) && !str_contains($value, ['jbatuka'])){
 	        		$cc_array[] = $value;
-        			echo "\t\t Added CCC {$value} \n";	        		
+        			echo "\t\t Added {$column_name} CCC {$value} \n";	        		
 	        	}
 	        	else if(str_contains($column_name, 'ccc') && !filter_var($value, FILTER_VALIDATE_EMAIL)){
 		        	echo "\t\t Email {$column_name} {$value} is invalid \n";	        		
