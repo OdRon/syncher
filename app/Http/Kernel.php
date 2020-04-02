40f0c323e485848fe47081cda6528931231c466b
Joel <joelkith@gmail.com>
2020-04-02 13:15:17 +0300

    Completed Covid Testing

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,9 +33,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-
-            // \App\Http\Middleware\TrimStrings::class,
-            // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         ],
 
         'api' => [
