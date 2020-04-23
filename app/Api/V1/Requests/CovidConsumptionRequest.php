a1188a6188fc1fa0f2b7eaa69ba33819bc60b567
Joshua Bakasa <baksajoshua09@gmail.com>
2020-04-24 02:03:42 +0300

    More updates

---
+++ app/Api/V1/Requests/CovidConsumptionRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Api\V1\Requests;
+
+use Dingo\Api\Http\FormRequest;
+// use App\Rules\BeforeOrEqual;
+
+class CovidConsumptionRequest extends FormRequest
+{
+    
+    public function rules()
+    {
+        return [];
+    }
+
+    public function authorize()
+    {
+    	$apikey = $this->headers->get('apikey');
+        $actual_key = env('COVID_KEY');
+        if($apikey != $actual_key || !$actual_key) return false;
+        else{
+            return true;
+        }
+    }
+
+    public function messages()
+    {
+        return [];
+    }
+}
