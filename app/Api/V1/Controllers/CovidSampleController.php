f65188752fec9dd2c155678e71e06fa11786c56a
Joel <joelkith@gmail.com>
2020-05-11 01:02:48 +0300

    Pull CIF samples

--- app/Api/V1/Controllers/CovidSampleController.php
+++ app/Api/V1/Controllers/CovidSampleController.php
@@ -128,8 +128,8 @@ class CovidSampleController extends Controller
     }
 
     public function cif_samples(){        
-        // $samples = CovidSample::where(['synched' => 0, 'lab_id' => 11])->whereNull('original_sample_id')->whereNull('receivedstatus')->with(['patient'])->get();
-        $samples = CovidSample::whereNotNull('cif_sample_id')->with(['patient'])->get();
+        $samples = CovidSample::where(['synched' => 0, 'lab_id' => 11])->whereNull('original_sample_id')->whereNull('receivedstatus')->with(['patient'])->get();
+        // $samples = CovidSample::whereNotNull('cif_sample_id')->with(['patient'])->get();
         return $samples;
     }
 
